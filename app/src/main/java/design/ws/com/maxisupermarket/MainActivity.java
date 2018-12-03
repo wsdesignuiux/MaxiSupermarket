4b1b0a3ba50b5d3b8e78c52eb13f924c56c878ee
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-03 10:12:56 +0530

    Initial commit

---
+++ app/src/main/java/design/ws/com/maxisupermarket/MainActivity.java
@@ -0,0 +1,13 @@
+package design.ws.com.maxisupermarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
